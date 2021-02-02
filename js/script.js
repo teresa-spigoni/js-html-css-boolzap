d63cfe7f33989d3321da7f917e3fb6d703d786a6
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-02 21:44:03 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 new Vue({
   el: '#root',
+  // DATA
   data: {
     contacts: [
       // MICHELE
@@ -93,11 +94,13 @@ new Vue({
     contactIndex: 0,
     myMessage: ''
   },
-
+  // METHODS
   methods: {
-    changeContact: function (index) {
+    // funzione per cambiare conversazione
+    changeConversation: function (index) {
       this.contactIndex = index
     },
+    // funzioni per generare l'ora -----------------------
     addZero: function (x) {
       if (x < 10) {
         x = ('0' + x);
@@ -109,43 +112,70 @@ new Vue({
       var hm = d.getHours() + ':' + this.addZero(d.getMinutes());
       return hm;
     },
+    // funzioni per le risposte automatiche --------------
+    answer: function (theText, time) {
+      setTimeout(() => {
+        this.contacts[this.contactIndex].messages.push({date: this.hour(), text: theText, status: 'received'})
+      }, time);
+    },
+    itIncludes: function (text) {
+      return this.myMessage.toLowerCase().includes(text)
+    },
     sendMessage: function () {
       if (this.myMessage !== '') {
+        // invio del messaggio
         this.contacts[this.contactIndex].messages.push({date: this.hour(), text: this.myMessage, status: 'sent'});
-
-        if (this.myMessage.toLowerCase().includes('ciao')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Ciao!', status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('come stai')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Io sto bene e tu?', status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('hey')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Hey!', status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('come va')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Tutto bene e tu?', status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('come ti chiami')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Mi chiamo ' + this.contacts[this.contactIndex].name + ', ti sei dimenticat*?' , status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('che ore sono')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Sono le ' + this.hour(), status: 'received'})
-          }, 2000);
-        } else if (this.myMessage.toLowerCase().includes('grazie')) {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Non c\'è di che!', status: 'received'})
-          }, 2000);
+        // -----------risposte automatiche-----------------
+        if (this.itIncludes('ciao')) {
+              this.answer('Ciao!', 1000);
+        } else if (this.itIncludes('hey')) {
+              this.answer('Hey!', 1000);
+        } else if (this.itIncludes('hello')) {
+              this.answer('Hello!', 1000);
+        } else if (this.itIncludes('hi')) {
+              this.answer('Hi!', 1000);
+        } else if (this.itIncludes('salve')) {
+              this.answer('Salve', 1000);
+        } else if (this.itIncludes('buongiorno')) {
+              this.answer('Buongiorno anche a te!', 3000);
+        } else if (this.itIncludes('good morning')) {
+              this.answer('Good morning dear!', 3000);
+        } else if (this.itIncludes('buonasera')) {
+              this.answer('Buonasera!', 2000);
+        } else if (this.itIncludes('good evening')) {
+              this.answer('Good evening!', 2000);
+        } else if (this.itIncludes('buon pomeriggio')) {
+              this.answer('Buon pome!', 2000);
+        } else if (this.itIncludes('good afternoon')) {
+              this.answer('Good afternoon!', 2000);
+        } else if (this.itIncludes('buonanotte')) {
+              this.answer('Buonanotte e sogni d\'oro!', 5000);
+        } else if (this.itIncludes('good night')) {
+              this.answer('Have a good good night!', 4000);
+        } else if (this.itIncludes('come stai')) {
+              this.answer('Io sto bene e tu?', 3000);
+        } else if (this.itIncludes('how are you')) {
+              this.answer('I\'m fine, what about you!', 4000);
+        } else if (this.itIncludes('come va')) {
+              this.answer('Tutto bene e tu?', 3000);
+        } else if (this.itIncludes('what\'s your name')) {
+              this.answer('My name is ' + this.contacts[this.contactIndex].name + ', do you remember?', 1000);
+        } else if (this.itIncludes('come ti chiami')) {
+              this.answer('Mi chiamo ' + this.contacts[this.contactIndex].name + ', ti sei dimenticat*?', 5000);
+        } else if (this.itIncludes('che ore sono')) {
+              this.answer('Sono le ' + this.hour(), 2000);
+        } else if (this.itIncludes('what time is it')) {
+              this.answer('It\'s ' + this.hour(), 2000);
+        } else if (this.itIncludes('grazie')) {
+              this.answer('Non c\'è di che!', 2000);
+        } else if (this.itIncludes('thank you')) {
+              this.answer('You\'re welcome!', 2000);
+        } else if (this.itIncludes('thanks')) {
+              this.answer('You\'re welcome!', 2000);
         } else {
-          setTimeout(() => {
-            this.contacts[this.contactIndex].messages.push({date: this.hour(), text: 'Ok', status: 'received'})
-          }, 2000);
+              this.answer('Ok', 100);
         }
+        // -----------risposte automatiche-----------------
         this.myMessage = '';
       }
     }
