2e2b650a615bb179b1e244590bb61d96a62849d0
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-03 21:24:10 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ new Vue({
   el: '#root',
   // DATA
   data: {
+    contactIndex: 0,
+    contactSearch: '',
+    myMessage: '',
     contacts: [
       // MICHELE
     	{
@@ -90,24 +93,26 @@ new Vue({
     			}
     		],
     	}
-    ],
-    contactIndex: 0,
-    myMessage: '',
-    contactSearch: ''
+    ]
   },
   // METHODS
   methods: {
-    // funzione per cercare un contatto
+    // metodo per cercare un contatto
     search: function (element) {
       if (element.name.toLowerCase().includes(this.contactSearch.toLowerCase()) || this.contactSearch === '') {
         return true;
       }
     },
-    // funzione per cambiare conversazione
+    // metodo per cambiare conversazione
     changeConversation: function (index) {
       this.contactIndex = index
     },
-    // funzioni per generare l'ora -----------------------
+    // metodo per cambiare l'ultimo accesso e l'ora dell'ultimo messaggio
+    lastMessageHour: function () {
+      let messages = this.contacts[this.contactIndex].messages;
+      return messages[this.contacts[this.contactIndex].messages.length - 1].date
+    },
+    // metodi per generare l'ora -----------------------
     addZero: function (x) {
       if (x < 10) {
         x = ('0' + x);
@@ -118,8 +123,8 @@ new Vue({
       let d = new Date();
       return d.getHours() + ':' + this.addZero(d.getMinutes());
     },
-    // funzioni per le risposte automatiche --------------
-    answer: function (theText, time) {
+    // metodi per le risposte automatiche --------------
+    reply: function (theText, time) {
       setTimeout(() => {
         this.contacts[this.contactIndex].messages.push({date: this.hour(), text: theText, status: 'received'})
       }, time);
@@ -129,66 +134,69 @@ new Vue({
     itIncludes: function (text) {
       return this.myMessage.toLowerCase().includes(text)
     },
+    // metodo per inviare il messaggio e ricevere una risposta automatica
     sendMessage: function () {
       if (this.myMessage !== '') {
         // invio del messaggio
         this.contacts[this.contactIndex].messages.push({date: this.hour(), text: this.myMessage, status: 'sent'});
         // -----------risposte automatiche-----------------
         if (this.itIncludes('ciao')) {
-              this.answer('Ciao!', 1000);
+              this.reply('Ciao!', 1000);
         } else if (this.itIncludes('hey')) {
-              this.answer('Hey!', 1000);
+              this.reply('Hey!', 1000);
         } else if (this.itIncludes('hello')) {
-              this.answer('Hello!', 1000);
+              this.reply('Hello!', 1000);
         } else if (this.itIncludes('salve')) {
-              this.answer('Salve', 1000);
+              this.reply('Salve', 1000);
         } else if (this.itIncludes('buongiorno')) {
-              this.answer('Buongiorno anche a te!', 3000);
+              this.reply('Buongiorno anche a te!', 3000);
         } else if (this.itIncludes('good morning')) {
-              this.answer('Good morning dear!', 3000);
+              this.reply('Good morning dear!', 3000);
         } else if (this.itIncludes('buonasera')) {
-              this.answer('Buonasera!', 2000);
+              this.reply('Buonasera!', 2000);
         } else if (this.itIncludes('good evening')) {
-              this.answer('Good evening!', 2000);
+              this.reply('Good evening!', 2000);
         } else if (this.itIncludes('buon pomeriggio')) {
-              this.answer('Buon pome!', 2000);
+              this.reply('Buon pome!', 2000);
         } else if (this.itIncludes('good afternoon')) {
-              this.answer('Good afternoon!', 2000);
+              this.reply('Good afternoon!', 2000);
         } else if (this.itIncludes('buonanotte')) {
-              this.answer('Buonanotte e sogni d\'oro!', 5000);
+              this.reply('Buonanotte e sogni d\'oro!', 5000);
         } else if (this.itIncludes('good night')) {
-              this.answer('Have a good good night!', 4000);
+              this.reply('Have a good good night!', 4000);
         } else if (this.itIncludes('come stai')) {
-              this.answer('Io sto bene e tu?', 3000);
+              this.reply('Io sto bene e tu?', 3000);
         } else if (this.itIncludes('how are you')) {
-              this.answer('I\'m fine, what about you!', 4000);
+              this.reply('I\'m fine, what about you!', 4000);
         } else if (this.itIncludes('come va')) {
-              this.answer('Tutto bene e tu?', 3000);
+              this.reply('Tutto bene e tu?', 3000);
         } else if (this.itIncludes('what\'s your name')) {
-              this.answer('My name is ' + this.contacts[this.contactIndex].name + ', do you remember?', 1000);
+              this.reply('My name is ' + this.contacts[this.contactIndex].name + ', do you remember?', 1000);
         } else if (this.itIncludes('come ti chiami')) {
-              this.answer('Mi chiamo ' + this.contacts[this.contactIndex].name + ', ti sei dimenticat*?', 5000);
+              this.reply('Mi chiamo ' + this.contacts[this.contactIndex].name + ', ti sei dimenticat*?', 5000);
         } else if (this.itIncludes('che ore sono')) {
-              this.answer('Sono le ' + this.hour(), 2000);
+              this.reply('Sono le ' + this.hour(), 2000);
         } else if (this.itIncludes('what time is it')) {
-              this.answer('It\'s ' + this.hour(), 2000);
+              this.reply('It\'s ' + this.hour(), 2000);
         } else if (this.itIncludes('grazie')) {
-              this.answer('Non c\'è di che!', 2000);
+              this.reply('Non c\'è di che!', 2000);
         } else if (this.itIncludes('thank you')) {
-              this.answer('You\'re welcome!', 2000);
+              this.reply('You\'re welcome!', 2000);
         } else if (this.itIncludes('thanks')) {
-              this.answer('You\'re welcome!', 2000);
+              this.reply('You\'re welcome!', 2000);
         } else {
-              this.answer('Ok', 100);
+              this.reply('Ok', 100);
         }
         // -----------risposte automatiche-----------------
         this.myMessage = '';
       }
     },
+    // metodo per attivare e disattivare il menu del messaggio
     activeMenu: function (index) {
       let menu = document.getElementsByClassName('menu')[index];
       menu.classList.toggle('d-block');
     },
+    // metodo per eliminare un messaggio
     deleteMessage: function (index) {
       this.contacts[this.contactIndex].messages.splice(index, 1);
     }
